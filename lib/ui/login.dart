47c92855ee346289c43091714323165c565bf301
apichart2425 <60070111@kmitl.ac.th>
2019-02-13 21:31:08 +0700

    80_percent_LoginScreen

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -5,7 +5,6 @@ import 'register.dart';
 class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return Login();
   }
 }
